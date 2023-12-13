73dccad86088687e4ffaeee5a568cac05e48672b
gowthamk007m <gowthamk007m@gmail.com>
2023-12-13 14:06:42 +0530

    modelupadate
    Signed-off-by: gowthamk007m <gowthamk007m@gmail.com>

--- placement_analysis/accounts/models.py
+++ placement_analysis/accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Jobs(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     job_title = models.CharField(max_length=100)
+    job_description = models.TextField()  # Fixed the missing field name
     company = models.CharField(max_length=50)
     salary = models.PositiveIntegerField(null=True, blank=True)
     vacancy = models.PositiveIntegerField(null=True, blank=True)
@@ -19,9 +20,24 @@ class UserModel(models.Model):
     profile = models.OneToOneField(User, on_delete=models.CASCADE)
     resume = models.FileField(upload_to='resumes/')
     is_approved = models.BooleanField(default=False)
-    applied_jobs = models.ForeignKey(
-        Jobs, on_delete=models.SET_NULL, null=True, blank=True)
+    applied_jobs = models.ForeignKey(Jobs, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.profile.username
 
+
+class CollegeModel(models.Model):
+    college = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CompanyModel(models.Model):
+    company = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class PlacementRequests(models.Model):
+    college = models.ForeignKey(CollegeModel, related_name="college_requests", on_delete=models.CASCADE)
+    company = models.ForeignKey(CompanyModel, related_name="company_requests", on_delete=models.CASCADE)
+    description = models.TextField()  
+    request_status = models.BooleanField(default=False)
+    request_date = models.DateField(auto_now_add=True)
+    
